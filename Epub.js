b9c5188fa4c145498fa3fed12a774bba94577567
GitBarton <51805876+GitBarton@users.noreply.github.com>
2019-07-24 22:28:56 -0400

    Add files via upload

---
+++ Epub.js
@@ -0,0 +1,8 @@
+
+var encoded = "Ne tenons pas compte de la politique de nos voisins du sud ici " + ":)";
+
+function play(){
+    var audio = document.getElementById("audio");
+    audio.play();
+
+              }
